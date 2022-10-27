771722f6ad0dd0e9d322a4c5a17aaaa29fde0203
Vitali Undra <vitalyundro@gmail.com>
2022-10-27 16:46:59 +0300

    fix

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
 import express from 'express';
 import cors from 'cors';
-
-const PORT = 3000;
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+
 app.get('/', (req, res) => {
 	res.json({ message: 'hello' });
 });
-app.listen(3000, err =>
-	err ? console.log(err) : console.log(`Server started on ${PORT}`)
+
+app.listen(PORT, err =>
+	err ? console.log(err) : console.log(`Server started on port ${PORT}`)
 );
