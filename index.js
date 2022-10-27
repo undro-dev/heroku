fd5c919a92a2633062e24a7d1aa5219362887f93
Vitali Undra <vitalyundro@gmail.com>
2022-10-27 16:26:38 +0300

    fix

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, err =>
+app.listen(3000, err =>
 	err ? console.log(err) : console.log(`Server started on ${PORT}`)
 );
